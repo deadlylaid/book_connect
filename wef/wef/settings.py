c12bcc31aa215c2ebf2474ea399bb5ae49e55fcb
deadlylaid <deadlylaid@gmail.com>
2016-07-16 17:04:34 +0900

    update : add library in requirement.txt , settings.url
    install django_extensions, debug_toolbar
    
    update : add library in settings.py

--- wef/wef/settings.py
+++ wef/wef/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'debug_toolbar',
+    'django_extensions',
+
 ]
 
 MIDDLEWARE_CLASSES = [
