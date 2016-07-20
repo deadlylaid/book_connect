2de2f15f636b1625a85fc456ec0c620dfc137dbb
deadlylaid <deadlylaid@gmail.com>
2016-07-20 17:10:44 +0900

    update : modify functional_test.py >> use unittest module

--- functional_test.py
+++ functional_test.py
@@ -1,17 +1,28 @@
 from selenium import webdriver
-
-browser = webdriver.Firefox()
-
-
-# 민수는 중고 도서공유 사이트가 있다는 소릴듣고 url접속을 한다.
-browser.get('http://localhost:8000/')
-
-# 웹 페이지는 헤더가 '남서울 도서공유'를 표시하고 있다.
-assert '남서울 도서공유' in browser.title
+import unittest
 
 
-# 민수는 회원가입을 하고싶다.
-# 하지만 회원가입이 없다.
+class NewVisitorTest(unittest.TestCase):
 
-# 민수는 브라우저를 종료한다.
-browser.quit()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_sign_up_this_website(self):
+
+        # 민수는 중고 도서공유 사이트가 있다는 소릴듣고 url접속을 한다.
+        self.browser.get('http://localhost:8000/')
+
+        # 웹 페이지는 헤더가 '남서울 도서공유'를 표시하고 있다.
+        self.assertIn('남서울 도서공유', self.browser.title)
+        self.fail('Finish the test!')
+
+        # 민수는 회원가입을 하고싶다.
+        # 하지만 회원가입이 없다.
+
+        # 민수는 브라우저를 종료한다.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
