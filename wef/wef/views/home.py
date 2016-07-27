91c7c73964b121238443c02c4d22dfc426ec3148
deadlylaid <deadlylaid@gmail.com>
2016-07-27 17:09:18 +0900

    update : add wef/templates >> home.html
    this template is '/' home page

--- wef/wef/views/home.py
+++ wef/wef/views/home.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("Hello world")
+
+    return render(
+            request,
+            'home.html',
+            context={},
+            )
