d4f17693134d88493c28b949bcbf92780a64ada1
deadlylaid <deadlylaid@gmail.com>
2016-08-03 18:08:42 +0900

    update  : add join_us.view POST request
    add POST request process code

--- wef/users/views/join_us.py
+++ wef/users/views/join_us.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from users.models import User
 
 
 def join_us(request):
-    return render(
-            request,
-            'users/joinus.html',
-            context={}
-            )
+
+    if request.method == "GET":
+        return render(
+                request,
+                'users/joinus.html',
+                context={}
+                )
+
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        phone = request.POST.get("phone")
+        password = request.POST.get("password")
+
+        created_user = User.objects.create_user(
+                username=username,
+                password=password,
+                phone=phone,
+                )
+
+        return redirect(
+                render(
+                    request,
+                    'users/home.html',
+                    context={}
+                    )
+                )
