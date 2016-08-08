8709a7e71ba19e562a9071eb2632663142daaf61
deadlylaid <deadlylaid@gmail.com>
2016-08-08 15:07:19 +0900

    update : add nickname in user model

--- wef/users/models/user.py
+++ wef/users/models/user.py
@@ -5,6 +5,12 @@ from django.db import models
 
 class User(AbstractUser):
 
+    nickname = models.CharField(
+            max_length=20,
+            null=True,
+            blank=True,
+            )
+
     phone = models.CharField(
             max_length=20,
             null=True,
