d713d227336d456ff999edbb3ea1103b4f6be713
deadlylaid <deadlylaid@gmail.com>
2016-08-10 13:57:44 +0900

    update : add item_post model

---
+++ wef/items/models/item_post.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.conf import settings
+
+
+class ItemPost(models.Model):
+
+    user = models.foreignKey(
+            settings.AUTH_USER_MODEL,
+            )
+
+    title = models.TextField(
+            )
+
+    created_at = models.DateTimeField(
+            auto_now_add=True,
+            )
+
+    updated_at = models.DateTimeField(
+            auto_now=True,
+            )
+
+    def __str__(self):
+        return self.title
