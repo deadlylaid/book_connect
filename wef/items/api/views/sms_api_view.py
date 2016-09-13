239e6d424b2fc1e32822b4ea625581decb9df2b8
deadlylaid <deadlylaid@gmail.com>
2016-09-13 15:30:24 +0900

    update : add sms_api_view.py in items/api/views it will used send sms

---
+++ wef/items/api/views/sms_api_view.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from items.models import BookList, ItemPost
+from items.api.serializers import BookListSerializer
+from items.tasks.send_buy_message import SendBuyMessageTask
+
+import os
+
+
+class SendBuySMSAPIView(APIView):
+
+    def post(self, request, *args, **kwargs):
+
+        sms = SendBuyMessageTask()
+        sms.run()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
