42be632ae76b03798efa1921a867eb4da5aa631b
deadlylaid <deadlylaid@gmail.com>
2016-09-28 14:51:17 +0900

    update : add password check when user create user when User create user, must double check using password, and checkpassword

--- wef/wef/static/js/users/joinus.js
+++ wef/wef/static/js/users/joinus.js
@@ -1,5 +1,19 @@
 (function(){
-    alert("joinus.js");
+
+    $("[name=checkpassword]").keyup(function(){
+        $password = $('input[name=password]');
+        $checkpassword = $('input[name=checkpassword]');
+        $pw_message = $('#pw_message')
+
+        if($checkpassword.val()!=$password.val()){
+            $pw_message.text('비밀번호와 다릅니다')
+            console.log("틀렸습니다");
+            console.log($checkpassword.val());
+            console.log($password.val());
+        }else{
+            $pw_message.text('비밀번호와 일치하였습니다.')
+        };
+    });
 })();
 
 
