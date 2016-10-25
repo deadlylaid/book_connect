f753d4d70b149b06a09407203c1ec55474c04358
deadlylaid <deadlylaid@gmail.com>
2016-10-25 21:18:09 +0900

    update : modify joinus.js

--- wef/wef/static/js/users/joinus.js
+++ wef/wef/static/js/users/joinus.js
@@ -1,24 +1,31 @@
 (function(){
 
+    $submit_button = $('#button');
+
     $("[name=checkpassword]").keyup(function(){
         $password = $('input[name=password]');
         $checkpassword = $('input[name=checkpassword]');
-        $pw_message = $('#pw_message')
+        var pw_message = document.getElementById('pw_message');
 
         if($checkpassword.val()!=$password.val()){
-            $pw_message.text('비밀번호와 다릅니다')
-            console.log("틀렸습니다");
-            console.log($checkpassword.val());
-            console.log($password.val());
+            var checkpw_message = '<p class="text-danger"><strong>잠깐! </strong> 비밀번호가 일치하지 않아요!</p>';
+            pw_message.innerHTML = checkpw_message;
+            
+            $submit_button.attr('disabled', true);
+            //console.log("틀렸습니다");
+            //console.log($checkpassword.val());
+            //console.log($password.val());
         }else{
-            $pw_message.text('비밀번호와 일치하였습니다.')
+            var checkpw_message = '<p class="text-success">비밀번호가 일치합니다.</p>';
+            pw_message.innerHTML = checkpw_message;
+            $submit_button.attr('disabled', false);
         };
     });
 
     $("[name=username]").focusout(function(){
         $username = $('input[name=username').val();
 
-        console.log($username)
+        //console.log($username)
 
         api_url = "/api/username/check/";
 
@@ -29,11 +36,14 @@
                 received_username: $username,
             },
             success:function(result){
-                        $check_username = $('#check_username')
+                        var check_username = document.getElementById("check_username");
                         if(result.overlap==true){
-                            $check_username.text('이미 존재하는 아이디입니다')
+                            var newElement = '<p class="text-danger"><strong>잠깐! </strong>이미 있는 아이디에요! 누구도 생각 못한 환상적인 아이디를 생각해보아요!</p>';
+                            check_username.innerHTML = newElement;
+                            $submit_button.attr('disabled', true);
                         }else{
-                            $check_username.text('멋진 아이디입니다!')
+                            var newElement = '<p class="text-success"><strong>정말 멋진 아이디에요! </strong></p>';
+                            check_username.innerHTML = newElement;
                         }
                     }
         
