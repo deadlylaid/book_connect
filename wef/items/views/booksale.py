d88b1abb00bf7a8c589fae2427cb7a0cc25f124e
deadlylaid <deadlylaid@gmail.com>
2016-11-01 17:39:17 +0900

    update : modify post need book name least one or more

--- wef/items/views/booksale.py
+++ wef/items/views/booksale.py
@@ -2,6 +2,7 @@ from django.views.generic import View
 from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.urlresolvers import reverse
+from django.contrib import messages
 
 from items.models import ItemPost, BookList
 
@@ -32,26 +33,39 @@ class BookSale(LoginRequiredMixin, View):
         book_names = request.POST.getlist('book')
         book_prices = request.POST.getlist('price')
 
-        saler = request.user
-
-        created_bookpost = ItemPost.objects.create(
-                user=saler,
-                title=title,
-                )
-
-        for i, names in enumerate(book_names):
-            BookList.objects.create(
-                    post=created_bookpost,
-                    booknumber=i+1,
-                    bookname=names,
-                    bookprice=book_prices[i],
+        # 등록된 책이 한권도 없을 때
+        if not book_names:
+            messages.add_message(
+                    request,
+                    messages.WARNING,
+                    "최소한 한권 이상의 도서가 등록되어야 합니다."
                     )
 
-        return redirect(
-                reverse(
-                    "postdetail",
-                    kwargs={
-                        'pk': created_bookpost.id,
-                        }
+            return redirect(reverse('booksale'))
+
+        else:
+            saler = request.user
+
+            created_bookpost = ItemPost.objects.create(
+                    user=saler,
+                    title=title,
                     )
-                )
+
+            for i, names in enumerate(book_names):
+                BookList.objects.create(
+                        post=created_bookpost,
+                        booknumber=i+1,
+                        bookname=names,
+                        bookprice=book_prices[i],
+                        )
+
+            return redirect(
+                    reverse(
+                        "postdetail",
+                        kwargs={
+                            'pk': created_bookpost.id,
+                            }
+                        )
+                    )
+
+
