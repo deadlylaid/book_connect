ed6ddbd880f2c2c09eb02ee67df952a4eee13b11
deadlylaid <deadlylaid@gmail.com>
2016-11-09 21:22:36 +0900

    update : add feature save staticfiles in S3 storages

--- wef/wef/settings/production.py
+++ wef/wef/settings/production.py
@@ -3,3 +3,10 @@ from .partials import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+STATICFILES_STORAGE = 'wef.storage.S3PipelineManifestStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_ACCESS_KEY_ID = 'AKIAIANK2NNJ47OHD5YA'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'nsuconnect'
