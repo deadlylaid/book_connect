22742d885606df022758a90424bc345a26aeae93
deadlylaid <deadlylaid@gmail.com>
2016-11-10 14:04:57 +0900

    update : pep8

--- wef/wef/settings/production.py
+++ wef/wef/settings/production.py
@@ -8,8 +8,6 @@ ALLOWED_HOSTS = ['*']
 STATICFILES_STORAGE = 'wef.storage.S3PipelineManifestStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-
