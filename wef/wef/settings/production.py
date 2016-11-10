471c04a1b230f915100e0c318ab123389cfbb2bf
deadlylaid <deadlylaid@gmail.com>
2016-11-10 13:08:59 +0900

    update : add S3 AWS key

--- wef/wef/settings/production.py
+++ wef/wef/settings/production.py
@@ -1,3 +1,4 @@
+import os
 from .partials import *
 
 DEBUG = False
@@ -7,6 +8,7 @@ ALLOWED_HOSTS = ['*']
 STATICFILES_STORAGE = 'wef.storage.S3PipelineManifestStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-AWS_ACCESS_KEY_ID = 'AKIAIANK2NNJ47OHD5YA'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'nsuconnect'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+
