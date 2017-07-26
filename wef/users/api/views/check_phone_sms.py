c2b046b327b40c93795aaeb0887e3bf22a4949ce
deadlylaid <deadlylaid@gmail.com>
2017-07-26 11:59:59 +0900

    update : send user phone number when search password

--- wef/users/api/views/check_phone_sms.py
+++ wef/users/api/views/check_phone_sms.py
@@ -12,24 +12,46 @@ class PhoneNumberCheck(APIView):
     def post(self, request, *args, **kwargs):
         received_phone_number = request.data.get('phone_number')
 
-        overlap_phone_number = User.objects.filter(
+        overlap_phone_number_user = User.objects.filter(
                 phone=received_phone_number,
                 )
 
         response_data = {}
 
-        if overlap_phone_number:
-            response_data['sms'] = False
+        if request.user.is_anonymous():
+            '''
+            ID, password search
+            '''
+            try:
+                searched_user_by_phone_number = overlap_phone_number_user[0]
+            except IndexError as e:
+                # 일치하는 회원정보 없음
+                #status는 추후 변경
+                response_data['result'] = False
+                response_data['id'] = None
+                return Response(response_data, status=status.HTTP_200_OK)
+
+            # 일치하는 회원정보 있음
+            response_data['result'] = True
+            response_data['id'] = searched_user_by_phone_number.username
             return Response(response_data, status=status.HTTP_200_OK)
+
         else:
-            response_data['sms'] = True
-            token = str(maketoken())
+            '''
+            join us
+            '''
+            if overlap_phone_number_user:
+                response_data['sms'] = False
+                return Response(response_data, status=status.HTTP_200_OK)
+            else:
+                response_data['sms'] = True
+                token = str(maketoken())
 
-            # 생성된 코드를 user model의 column에 넣는다.
-            request.user.certification_code = token
-            request.user.save()
+                # 생성된 코드를 user model의 column에 넣는다.
+                request.user.certification_code = token
+                request.user.save()
 
-            task = SendPhoneCheckSMSTask()
-            task.delay(received_phone_number, token)
+                task = SendPhoneCheckSMSTask()
+                task.delay(received_phone_number, token)
 
-            return Response(response_data, status=status.HTTP_200_OK)
+                return Response(response_data, status=status.HTTP_200_OK)
